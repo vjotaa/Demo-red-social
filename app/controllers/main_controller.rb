d4404a62c9d89f4cb7e907b72d104cbfabf23e0e
Victor Manrique <vjotaa@gmail.com>
2016-10-11 00:22:47 -0400

    start a social red

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,14 @@
+class MainController < ApplicationController
+
+  def home
+  end
+
+  def unregistered
+  end
+
+  protected
+	  def set_layout
+	  	return 'landing' if action_name == "unregistered"
+	  	super
+	  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+  get 'main/home'
+  authenticated :user do
+  	root 'main#home'
+  end
+
+  unauthenticated :user do
+  	root 'main#unregistered'
+  end
+  
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
